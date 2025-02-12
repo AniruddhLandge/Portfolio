3c8cb942845ac34c6bf29eb851547fc971bff53d
AniruddhLandge <landgeaniruddha@gmail.com>
2025-02-12 08:36:20 +0530

    first commit

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -7,11 +7,16 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
 
     Coffee_ShopDesc: "A coffee shop management project involves tracking inventory, managing orders, scheduling staff, and handling customer feedback. It aims to improve operational efficiency, customer satisfaction, and profitability in a coffee shop business.",
+    Coffee_ShopGithub :"https://github.com/AniruddhLandge/Coffee-Shop-Project-With-DB",
+    Coffee_ShopWebsite : "https://AniruddhLandge.github.io/CoffeeShop/",
 
     Weather_AppDesc:"A weather app offers real-time weather forecasts, including temperature, humidity, wind speed, and other conditions. It helps users track and plan activities based on weather information for various locations worldwide.",
-    
-    PortfolioDesc:"A portfolio showcases an individual's work, skills, and accomplishments, typically in creative fields. It includes projects, designs, achievements, and professional experiences, highlighting expertise to potential clients, employers, or collaborators.",
+    Weather_AppGithub :"https://github.com/AniruddhLandge/weather",
+    Weather_AppWebsite : "https://aniruddhlandge.github.io/weather/",
 
+    PortfolioDesc:"A portfolio showcases an individual's work, skills, and accomplishments, typically in creative fields. It includes projects, designs, achievements, and professional experiences, highlighting expertise to potential clients, employers, or collaborators.",
+    PortfolioGithub :"https://github.com/AniruddhLandge/Portfolio",
+    PortfolioWebsite : "https://github.com/AniruddhLandge/Portfolio",
   }
 
   let show ='';
@@ -28,7 +33,12 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
             <br />
             {desc[projectName + 'Desc']}
             <br />
-
+<a style={{display:show}} href={desc[projectName+'Github']} target='_blank'>
+<button className='projectbtn'><FaGithub/>Github</button>
+</a>
+<a href={desc[projectName + 'Website']} target='_blank'>
+<button className='projectbtn'><CgFileDocument/>Demo</button>
+</a>
         </div>
     </div>
   )
